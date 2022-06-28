a43f215a0042233a9cfa547b3d2fa23a4609ee1a
Jeremy Vollant <j.vollant@gmail.com>
2022-06-28 15:17:37 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
+
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const addUserHandler = (newUser) => {
+    setUsers((prevState) => [...prevState, newUser]);
+  };
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={users}></UsersList>
+    </div>
+  );
+};
+
+export default App;
