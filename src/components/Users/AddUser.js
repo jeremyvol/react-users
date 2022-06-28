c82aa7136367330b8fc08b0fd4c8dd4c108cfda1
Jeremy Vollant <j.vollant@gmail.com>
2022-06-28 21:48:19 +0200

    Wrapper

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -5,6 +5,7 @@ import classes from './AddUser.module.css';
 import Button from '../UI/Button';
 import Card from '../UI/Card';
 import ErrorModal from '../UI/ErrorModal';
+import Wrapper from '../Helpers/Wrapper';
 
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState('');
@@ -47,7 +48,7 @@ const AddUser = (props) => {
   };
 
   return (
-    <div>
+    <Wrapper>
       {error && (
         <ErrorModal
           title={error.title}
@@ -74,7 +75,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 };
 
